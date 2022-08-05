568f74ec0f0a20e76654861d5eee3e96f546fa3c
Joel Shin <joel@fluxxlabs.com>
2022-08-05 10:36:29 -0700

    cleanedup specs

--- backend/spec/requests/guitar_spec.rb
+++ backend/spec/requests/guitar_spec.rb
@@ -1,6 +1,40 @@
 require 'rails_helper'
 
 describe 'Guitar', type: :request do
+  describe 'POST /api/v1/guitars' do
+    context 'given valid params' do
+      it 'should create a guitar' do
+        expect {
+          post '/api/v1/guitars', params: {
+            guitar: { name: 'gibson 444', price: 1200}
+          }
+        }.to change { Guitar.count }.from(0).to(1)
+
+        data = JSON.parse(response.body)
+        expect(response).to have_http_status(:ok)
+        expect(data['id']).to eq(1)
+        expect(data['name']).to eq('gibson 444')
+        expect(data['price']).to eq(1200)
+      end
+    end
+
+    context 'given invalid params' do
+      it 'should not create a guitar' do
+        post '/api/v1/guitars', params: {
+          guitar: {
+            name: '',
+            price: 1200
+          }
+        }
+
+        expect(response).to have_http_status(:bad_request)
+        expect(Guitar.count).to eq(0)
+        data = JSON.parse(response.body)
+        expect(data['message']).to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
   describe 'DELETE /api/v1/guitars/:id' do
     context 'given valid id' do
       it 'should destroy guitar' do
@@ -82,41 +116,6 @@ describe 'Guitar', type: :request do
         expect(data).to eq(
           {'id' => 1, 'name' => 'fender 123', 'price' => 500},
         )
-      end
-    end
-  end
-
-  describe 'POST /api/v1/guitars' do
-    context 'given invalid params' do
-      it 'should not create a guitar' do
-        post '/api/v1/guitars', params: {
-          guitar: {
-            name: '',
-            price: 1200
-          }
-        }
-
-        expect(Guitar.count).to eq(0)
-        data = JSON.parse(response.body)
-        expect(data['message']).to match(/Validation failed: Name can't be blank/)
-      end
-    end
-
-    context 'given invalid params' do
-      it 'should create a guitar' do
-        expect {
-          post '/api/v1/guitars', params: {
-            guitar: {
-              name: 'gibson 444',
-              price: 1200
-            }
-          }
-        }.to change { Guitar.count }.from(0).to(1)
-
-        data = JSON.parse(response.body)
-        expect(data['id']).to eq(1)
-        expect(data['name']).to eq('gibson 444')
-        expect(data['price']).to eq(1200)
       end
     end
   end
